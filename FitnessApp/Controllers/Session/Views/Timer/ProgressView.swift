1e666e453889e8df4a6a9a8b73a6df68d7066144
Aleksander <silvershtain@protonmail.com>
2022-12-27 13:24:01 +0700

    add TimerView

---
+++ FitnessApp/Controllers/Session/Views/Timer/ProgressView.swift
@@ -0,0 +1,16 @@
+//
+//  ProgressView.swift
+//  FitnessApp
+//
+//  Created by Aleksandr on 27.12.2022.
+//
+
+import UIKit
+
+extension TimerView {
+    
+    final class ProgressView: UIView {        
+        func drawProgress(with percent: CGFloat) {
+        }
+    }
+}
---
+++ FitnessApp/Controllers/Session/Views/Timer/TimerView.swift
@@ -0,0 +1,41 @@
+//
+//  TimerView.swift
+//  FitnessApp
+//
+//  Created by Aleksandr on 27.12.2022.
+//
+
+import UIKit
+
+final class TimerView: WABaseInfoView {
+    
+    private let progressView: ProgressView = {
+        let progressView = ProgressView()
+        progressView.drawProgress(with: 0)
+        
+        return progressView
+    }()
+    
+}
+
+extension TimerView {
+    override func setupViews() {
+        super.setupViews()
+        setupView(progressView)
+    }
+    
+    override func constraintViews() {
+        super.constraintViews()
+        NSLayoutConstraint.activate([
+            progressView.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -40),
+            progressView.leadingAnchor.constraint(equalTo: leadingAnchor,constant: 40),
+            progressView.topAnchor.constraint(equalTo: topAnchor, constant: 40),
+            progressView.heightAnchor.constraint(equalTo: progressView.widthAnchor)
+        ])
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+        progressView.backgroundColor = .red
+    }
+}
